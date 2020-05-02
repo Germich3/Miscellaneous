11d67d9d2e3f907113967bd39b473b0d08ec4042
Germich3 <germich3@gmail.com>
2020-05-02 12:05:11 +0200

    stuff

---
+++ Minecraft/ComputerCraft/programs/lock.lua
@@ -0,0 +1,48 @@
+--IMPORTS--
+os.loadAPI("/disk/apis/cryptex.lua")
+
+--LIMIAR PANTALLA--
+shell.run("clear")
+
+--PARAMETROS--
+local tArgs = {...}
+if #tArgs ~= 5 then
+    print("-----------------------")
+    print("ERROR DE INICIALIZACION")
+    print("-----------------------")
+    print("")
+    print("Se requieren los siguientes parametros:")
+    print("")
+    print("  - Parametro 1: Posicion del modem wireless")
+    print("  - Parametro 2: Puerto local")
+    print("  - Parametro 3: Posicion del envio de redstone")
+    print("  - Parametro 4: Tiempo del pulso en segundos")
+    print("  - Parametro 5: Contraseña")
+    print("")
+    print("Ejemplo:")
+    print("")
+    print("  - lock back 8080 left 5 password")
+    print("")
+    return    
+end
+
+--VARIABLES--
+local rdnum = math.random(1024, 2048)
+local modem = peripheral.wrap(tArgs[1])
+local port = tonumber(tArgs[2])
+local pside = tArgs[3]
+local ptime = tonumber(tArgs[4])
+local checksum = cryptex.checksum(tArgs[5], rdnum)
+
+--MAIN--
+modem.closeAll()
+modem.open(port)
+while true do
+    local ev, sd, fr, rfr, msg, ds = os.pullEvent("modem_message")
+    local input = cryptex.checksum(msg, rdnum)
+    if checksum == input then 
+        redstone.setOutput(pside, true)
+        os.sleep(ptime)
+        redstone.setOutput(pside, false)
+    end
+end
